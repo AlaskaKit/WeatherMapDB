0228e022471d086ff4b1b25ea14e539c52532b6d
AlaskaKit <alaska_kit@vivaldi.net>
2020-11-13 19:04:46 +0300

    5th commit: copying script fixed, dump executed

--- weather_picking_script.py
+++ weather_picking_script.py
@@ -29,8 +29,8 @@ def select_cities(low_temp: float = 14.0, high_temp: float = 21.0) -> None:
     # creating table in case it doesn't exits
     try:
         cursor.execute(f"CREATE TABLE IF NOT EXISTS public.{SORTED_W} "
-                       f"AS TABLE {UNSORTED_W} WITH NO DATA")
-                       
+                       f"(LIKE {UNSORTED_W} INCLUDING ALL)")
+        
     except psycopg2.Error as err:
         print(err.pgerror, file=sys.stderr)
         sys.exit(1)
@@ -38,7 +38,18 @@ def select_cities(low_temp: float = 14.0, high_temp: float = 21.0) -> None:
     # copying values
     try:
         cursor.execute(f"INSERT INTO {SORTED_W} SELECT * FROM {UNSORTED_W} "
-                       f"WHERE temperature > {low_temp} OR temperature < {high_temp}")
+                       f"WHERE temperature > {low_temp} AND temperature < {high_temp} "
+                       f"ON CONFLICT (city_id) DO UPDATE "
+                       f"SET city_id = excluded.city_id, "
+                       f"city_name = excluded.city_name, "
+                       f"weather_description = excluded.weather_description, "
+                       f"temperature = excluded.temperature, "
+                       f"pressure = excluded.pressure, "
+                       f"humidity = excluded.humidity, "
+                       f"visibility = excluded.visibility, "
+                       f"wind_speed = excluded.wind_speed, "
+                       f"wind_dir = excluded.wind_dir, "
+                       f"clouds_all = excluded.clouds_all")
 
     except psycopg2.Error as err:
         print(err.pgerror, file=sys.stderr)
